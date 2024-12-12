ec987b6832e9bad076003769fdd94e2942788114
Melisa SUBAŞI <120743510+msubasii@users.noreply.github.com>
2024-12-12 12:03:50 +0300

    Update soundRecord.py

--- soundRecord.py
+++ soundRecord.py
@@ -4,14 +4,30 @@ import wave
 import time
 from tkinter import Tk, Label
 from threading import Thread, Event
+import os
 
 # Ses ayarları
 CHUNK = 1024  # Örnek başına okunan veri
 FORMAT = pyaudio.paInt16  # 16-bit ses formatı
 CHANNELS = 1  # Mono
 RATE = 44100  # Örnekleme hızı (Hz)
-DURATION = 20  # Kayıt süresi (saniye)
-WAIT_TIME = 600  # Tekrarlama süresi (saniye)
+
+#DENEME AMAÇLI
+DURATION = 5  # Kayıt süresi (saniye)
+WAIT_TIME = 25  # Tekrarlama süresi (saniye)
+FOLDER_DURATION = 4 * 30  # Yeni klasör için süre (saniye)
+
+#kayıt formatı
+#DURATION = 10  # Kayıt süresi (saniye)
+#WAIT_TIME = 600  # Tekrarlama süresi (saniye)
+#FOLDER_DURATION = 4 * 3600  # Yeni klasör için süre (saniye)
+
+def create_folder():
+    """Tarih ve saate göre yeni bir klasör oluşturur ve yolunu döndürür."""
+    folder_name = time.strftime("%Y"+"."+"%m"+"."+"%d-%H"+":"+"%M"+":"+"%S")  # Zaman damgası
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    return folder_name
 
 def record_audio_to_wav(duration, filename, update_label_func):
     audio = pyaudio.PyAudio()
@@ -36,10 +52,16 @@ def record_audio_to_wav(duration, filename, update_label_func):
         wf.writeframes(b''.join(frames))
 
 def start_recording_thread(label, stop_event):
+    next_folder_time = time.time() + FOLDER_DURATION
+    current_folder = create_folder()
+    
     while not stop_event.is_set():
+        if time.time() >= next_folder_time:
+            current_folder = create_folder()
+            next_folder_time = time.time() + FOLDER_DURATION
+        
         current_time = time.strftime("%Y%m%d-%H%M%S")  # Zaman damgası
-        wav_filename = f'output_{current_time}.wav'
-
+        wav_filename = f'{current_folder}/record_{current_time}.wav'
         record_audio_to_wav(DURATION, wav_filename, label.config)
 
         for remaining in range(WAIT_TIME - DURATION, 0, -1):
